a5509db359f80aea6775d0dde16c9a8c4fa52a38
Prince Nishad <pn3147399@gmail.com>
2025-08-22 08:25:30 +0530

    link added

--- src/store/baseApi.js
+++ src/store/baseApi.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api1.team04.site/api/v1",
-    // baseUrl: "http://localhost:5000/api/v1",
+    // baseUrl: "https://api1.team04.site/api/v1",
+    baseUrl: "http://localhost:5000/api/v1",
     credentials: "include",
   }),
   tagTypes: ["User", "Tournament", "Order", "Admin", "Withdraw"],
